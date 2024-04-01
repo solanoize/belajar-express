c2fbca1ff4a69e8c7c64fb6344f6db9b49211e9f
StuvidLabs <yanwarsolah@gmail.com>
2024-04-01 19:37:07 +0700

    step 02: koneksi ke database

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { BarangRouter } = require("./barang/barang.router");
 const { ROUTER_BASE_BARANG } = require("./barang/barang.config");
+const { MongoDBConnection } = require("./libs/lib.database");
 
 const app = express();
+
+MongoDBConnection();
 
 app.use(express.json());
 
